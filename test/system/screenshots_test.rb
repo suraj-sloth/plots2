50f02efe01e2966cc238cfa5aae1f9f2075ae071
Cess <cessmbuguar@gmail.com>
2019-11-27 16:56:55 +0300

    change route path

--- test/system/screenshots_test.rb
+++ test/system/screenshots_test.rb
@@ -244,7 +244,7 @@ class ScreenshotsTest < ApplicationSystemTestCase
     fill_in("username-login", with: "steff1")
     fill_in("password-signup", with: "secretive")
     click_on "Log in"
-    visit '/post/simple'
+    visit '/simple_post'
     find('button#location_button').click
     take_screenshot
   end
